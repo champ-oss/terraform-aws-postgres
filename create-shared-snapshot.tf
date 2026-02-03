5d7c7d914755a6df46622153bed88e7275f8236d
Phillip Langley <philliplangley@champmbplangley.lan>
2026-02-03 10:58:14 -0600

    update shared snapshot version process

--- create-shared-snapshot.tf
+++ create-shared-snapshot.tf
@@ -1,7 +1,27 @@
+locals {
+  snapshot_version_suffix = "-v${var.shared_snapshot_version}"
+
+  max_snapshot_id_length = 63
+
+  # Reserve space for the version suffix
+  max_cluster_id_length = local.max_snapshot_id_length - length(local.snapshot_version_suffix)
+
+  snapshot_base = substr(
+    aws_rds_cluster.this[0].id,
+    0,
+    local.max_cluster_id_length
+  )
+}
+
 resource "aws_db_cluster_snapshot" "this" {
-  count                          = var.enable_shared_snapshot && var.enabled ? 1 : 0
-  db_cluster_identifier          = aws_rds_cluster.this[0].id
-  db_cluster_snapshot_identifier = var.db_cluster_snapshot_identifier != null ? var.db_cluster_snapshot_identifier : substr("${aws_rds_cluster.this[0].id}-v${var.shared_snapshot_version}", 0, 63)
-  shared_accounts                = var.shared_accounts_snapshot
-  tags                           = merge(local.tags, var.tags)
+  count = var.enable_shared_snapshot && var.enabled ? 1 : 0
+
+  db_cluster_identifier = aws_rds_cluster.this[0].id
+
+  db_cluster_snapshot_identifier = var.db_cluster_snapshot_identifier != null
+    ? var.db_cluster_snapshot_identifier
+    : "${local.snapshot_base}${local.snapshot_version_suffix}"
+
+  shared_accounts = var.shared_accounts_snapshot
+  tags            = merge(local.tags, var.tags)
 }
