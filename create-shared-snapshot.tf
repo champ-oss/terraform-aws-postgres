e9efb866a1285bab26dba97d08161abf0a6c2e0a
Phillip Langley <philliplangley@champmbplangley.lan>
2026-05-21 09:58:39 -0500

    update prefix

--- create-shared-snapshot.tf
+++ create-shared-snapshot.tf
@@ -6,12 +6,19 @@ resource "aws_db_cluster_snapshot" "this" {
     var.db_cluster_snapshot_identifier != null
     ? var.db_cluster_snapshot_identifier
     : substr(
-        "${formatdate("YYYYMMDDhhmmss", timestamp())}-${aws_rds_cluster.this[0].id}",
-        0,
-        63
-      )
+    "${random_string.snapshot[0].result}-${aws_rds_cluster.this[0].id}",
+    0,
+    63
+  )
   )
 
   shared_accounts = var.shared_accounts_snapshot
   tags            = merge(local.tags, var.tags)
+}
+
+resource "random_string" "snapshot" {
+  count   = var.enable_shared_snapshot && var.enabled ? 1 : 0
+  length  = 5
+  special = false
+  upper   = false
 }
